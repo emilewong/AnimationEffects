80144dd24eb3ad6363e495f94368073fbe610f82
Emile Wong <emilewong@outlook.com>
2021-05-12 20:59:57 +0800

    Initial Commit

---
+++ AnimationExercises/AnimationExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExercisesApp.swift
+//  AnimationExercises
+//
+//  Created by Emile Wong on 12/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
